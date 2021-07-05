69a4a7eae5e378e6aae02fd5b81808b9ea5d8d38
AidanJP <aidanpendlebury@gmail.com>
2021-07-05 18:50:14 +0100

    Basic UI.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let context = CoreDataManager().persistentContainer.viewContext
+    @State private var activityName: String = ""
+    @State private var activeLogRecord: LogRecord?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(activeLogRecord != nil ? "Recording" : "")
+            TextField("Description", text: $activityName)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button("\(activeLogRecord != nil ? "Stop" : "Start") recording") {
+                buttonPressed()
+            }
+        }
+    }
+    
+    func buttonPressed() {
+        
+        if activeLogRecord == nil {
+            let newLogRecord = LogRecord(context: context)
+            newLogRecord.activityName = activityName
+            newLogRecord.startTime = Date()
+            activeLogRecord = newLogRecord
+        } else {
+            activeLogRecord?.endTime = Date()
+            do {
+                try context.save()
+            } catch {
+                print("Error")
+            }
+        }
     }
 }
 
