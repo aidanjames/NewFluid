f728ea986166549ff16124d50713a59d8d1203e5
AidanJP <aidanpendlebury@gmail.com>
2021-07-15 18:43:08 +0100

    Begin extraction of the activity view.

---
+++ Shared/Views/ActivityView.swift
@@ -0,0 +1,53 @@
+//
+//  ActivityView.swift
+//  NewFluid
+//
+//  Created by Aidan Pendlebury on 15/07/2021.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    
+    var logRecord: LogRecord
+    
+    @Binding var refreshRequired: Bool
+    
+    
+    var body: some View {
+        HStack {
+            
+            Text(logRecord.activityName ?? "").bold()
+            Text(logRecord.startTime?.formatted(.dateTime.year().day().month().hour().minute().second()) ?? "").fontWeight(.light)
+            Spacer()
+            if logRecord.endTime == nil {
+                HStack {
+                    Text("\(logRecord.startTime!.secondsSinceDate().secondsToHoursMinsSecs())")
+                        .font(.system(.body, design: .monospaced))
+                    Button {
+                        logRecord.endTime = Date()
+                        refreshRequired.toggle()
+                    } label: {
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.red)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ActivityView_Previews: PreviewProvider {
+
+    
+    static var previews: some View {
+        
+        let coreDM = CoreDataManager()
+        let logRecord = LogRecord(context: coreDM.persistentContainer.viewContext)
+        logRecord.activityName = "Discovery"
+        logRecord.startTime = Date()
+        
+        return ActivityView(logRecord: logRecord, refreshRequired: .constant(false))
+    }
+}
