2ec780c83f19d3ed9f7168b7deea5d5898e31a67
AidanJP <aidanpendlebury@gmail.com>
2021-08-16 18:40:41 +0100

    Fixed various issues with the pomodoro view.

--- Shared/Views/PomodoroView.swift
+++ Shared/Views/PomodoroView.swift
@@ -25,22 +25,24 @@ struct PomodoroView: View {
         }
     }
     
-    var progress: Double {
+    var progress: CGFloat {
         guard let startTime = currentSessionStartTime else { return 0 }
         guard let endTime = currentSessionEndTime else { return  0 }
-        return (abs(startTime.secondsSinceDate()) / endTime.timeIntervalSince(startTime)) * 100
+        if Date() < endTime {
+            return (abs(startTime.secondsSinceDate()) / endTime.timeIntervalSince(startTime))
+        }
+        return 1.0
         
     }
     
     var body: some View {
         
         GeometryReader { geo in
-            
             VStack {
                 HStack {
-                    if let endTime = currentSessionEndTime {
-                        Spacer().frame(width: Date() < endTime ? ((geo.size.width * progress / 100) - 30) : geo.size.width)
-                    }
+                    Spacer()
+                        .frame(maxWidth: geo.size.width * progress >= 30 ? geo.size.width * progress - 30 : 0)
+                        .background(Color.red)
                     if let startTime = currentSessionStartTime {
                         if let endTime = currentSessionEndTime {
                             if endTime > Date() {
@@ -49,24 +51,21 @@ struct PomodoroView: View {
                                 Text((endTime.timeIntervalSince(startTime)).secondsToHoursMinsSecs())
                             }
                         }
-                        
-
                     } else {
                         Text("")
                     }
                     Spacer()
-                    if progress < 93 {
+                    if progress < 0.93 {
                         if let startTime = currentSessionStartTime, let endTime = currentSessionEndTime {
                             Text((endTime.timeIntervalSince(startTime)).secondsToHoursMinsSecs())
                         } else {
                             Text("")
                         }
-                        
                     }
                 }
                 .font(.caption2)
-                ProgressView(value: progress, total: 100)
-                    .foregroundColor(.red)
+                ProgressView(value: progress)
+                    .frame(maxWidth: geo.size.width)
             }
         }
         .frame(height: 10)
@@ -79,7 +78,6 @@ struct PomodoroView_Previews: PreviewProvider {
         PomodoroView(currentSessionType: .constant(.regularSession), currentSessionStartTime: .constant(Date().addingTimeInterval(-400)), currentSessionEndTime: .constant(Date().addingTimeInterval(100)))
     }
 }
-
 
 
 
